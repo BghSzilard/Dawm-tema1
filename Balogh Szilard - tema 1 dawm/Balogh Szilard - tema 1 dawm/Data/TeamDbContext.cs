95abfa4c094c09bd11ad5e0efa339f66ae70f8a2
BghSzilard <105720992+BghSzilard@users.noreply.github.com>
2023-03-20 18:37:33 +0200

    Created TeamDbContext

---
+++ Balogh Szilard - tema 1 dawm/Balogh Szilard - tema 1 dawm/Data/TeamDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.KeyVault.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Balogh_Szilard___tema_1_dawm.Data
+{
+    public class TeamDbContext : DbContext
+    {
+        public TeamDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Contact> contacts { get; set; }
+    }
+}
